11d793437c0725356027c1cc9aa77d9c44316ab3
elfsvet <stepanmatysik@gmail.com>
2022-10-24 10:37:31 -0400

    added new schema and updated the rest for get and create. Also added middleware for express json.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -6,30 +6,95 @@ const BootcampSchema = new mongoose.Schema({
     required: [true, 'Please add a name'],
     unique: true,
     trim: true,
-    maxLength: [50, 'Name should be less than 50 characters'],
+    maxlength: [50, 'Name can not be more than 50 characters']
   },
   slug: String,
   description: {
     type: String,
-    required: [true, 'Please add a name'],
-    maxLength: [500, 'Name should be less than 50 characters'],
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'Description can not be more than 500 characters']
   },
   website: {
     type: String,
     match: [
-      /https?:\/\/(www\.)?[a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*) /, //regex for https http websites js regex url whats a good regex f
-      'Please use a valid URL with HTTP or HTTPS',
-    ],
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please use a valid URL with HTTP or HTTPS'
+    ]
   },
   phone: {
     type: String,
-    match: [20, 'Phone number can not be longer than 20 characters'],
+    maxlength: [20, 'Phone number can not be longer than 20 characters']
   },
   email: {
     type: String,
     match: [
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please add a valid email',
-    ],
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point']
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
+  careers: {
+    // Array of strings
+    type: [String],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other'
+    ]
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating must can not be more than 10']
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg'
+  },
+  housing: {
+    type: Boolean,
+    default: false
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
   },
 });
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
