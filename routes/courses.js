6e618d2ed832cdda3961b7d2b3f6b86e96d39828
elfsvet <stepanmatysik@gmail.com>
2022-10-30 21:13:35 -0400

    created crud for courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { getCourses } = require('../controllers/courses');
+const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require('../controllers/courses');
 
 // mergeParams set to true to merge url
 // Preserve the req.params values from the parent router. If the parent and the child have conflicting param names, the child’s value take precedence.
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse)
 
 module.exports = router;
