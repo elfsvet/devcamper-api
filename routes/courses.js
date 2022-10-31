8f0c58423b4e4741abc5a811365b2043910387ef
elfsvet <stepanmatysik@gmail.com>
2022-10-31 10:11:54 -0400

    moved filtering and advanced results into a middleware and used it in bootcamp and courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,12 +1,27 @@
 const express = require('express');
 
-const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require('../controllers/courses');
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../controllers/courses');
+
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
 
 // mergeParams set to true to merge url
 // Preserve the req.params values from the parent router. If the parent and the child have conflicting param names, the child’s value take precedence.
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse)
+router
+  .route('/')
+  .get(
+    advancedResults(Course, { path: 'bootcamp', select: 'name description' }),
+    getCourses
+  )
+  .post(addCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
